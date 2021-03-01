a6af33eafd5c8a85170e610a5bbbffdcd7027c3c
sarayuth2019 <75245926+sarayuth2019@users.noreply.github.com>
2021-03-02 01:44:35 +0700

    edit picture

--- src/main/java/com/example/Aplication/model/service/CartRepository.java
+++ src/main/java/com/example/Aplication/model/service/CartRepository.java
@@ -4,9 +4,10 @@ package com.example.Aplication.model.service;
 import com.example.Aplication.model.table.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
-
+@Transactional
 public interface CartRepository extends JpaRepository<Cart,Integer> {
     Optional<Cart>findById(int id);
     List<Cart> findByName(String name);
