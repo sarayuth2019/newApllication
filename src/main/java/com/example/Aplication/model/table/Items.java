71f8d12bb55d09ab2237214547a84980582d5e92
sarayuth2019 <75245926+sarayuth2019@users.noreply.github.com>
2021-07-05 14:21:24 +0700

    edit table customer add columns name_store,description_store

--- src/main/java/com/example/Aplication/model/table/Items.java
+++ src/main/java/com/example/Aplication/model/table/Items.java
@@ -24,8 +24,14 @@ public class Items {
     private int price;
     @Column
     private int price_sell;
+    @Column
+    private int count;
+    @Column
+    private int count_request;
     @Column(name = "user_id")
     private int user;
+    @Column(name = "customer_id")
+    private int customer;
     @Column
     @JsonFormat(timezone = "Asia/Bangkok",pattern = "HH.mm.ss dd-MMM-yyyy")
     private Date date_begin;
