72137b67e6eb96ac345c4edaa291889f6b0a041a
sarayuth2019 <75245926+sarayuth2019@users.noreply.github.com>
2021-08-06 13:28:27 +0700

    add function deleteImage in ImageController.class

--- src/main/java/com/powergroup/model/service/ImageRepository.java
+++ src/main/java/com/powergroup/model/service/ImageRepository.java
@@ -3,9 +3,10 @@ package com.powergroup.model.service;
 import com.powergroup.model.table.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
-
+@Transactional
 public interface ImageRepository extends JpaRepository<Image,Integer> {
 //    Image findByMarketId(int marketId);
     List<Image> findByItemId(int itemId);
