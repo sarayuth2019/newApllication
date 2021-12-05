a178b5d0632dfd9b7a8d8d4914822defeb42b675
sarayuth2019 <75245926+sarayuth2019@users.noreply.github.com>
2021-12-05 14:46:15 +0700

    แก้ไข list ของ PayController ในส่วนของ findByUserIdAndItemId แก้ไข Array Response ตัดตัวซ้ำเรียงจากน้อยไปมาก Drop Table Repository Controller UserBackupNotify.java และ Order.java

--- src/main/java/com/powergroup/controller/UserBackupNotifyController.java
+++ src/main/java/com/powergroup/controller/UserBackupNotifyController.java
@@ -1,49 +1,49 @@
-package com.powergroup.controller;
-
-import com.powergroup.model.bean.APIResponse;
-import com.powergroup.model.service.UserBackupNotifyRepository;
-import com.powergroup.model.table.UserBackupNotify;
-import com.powergroup.util.ContextUtil;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-@RequestMapping("/UserBackup")
-public class UserBackupNotifyController {
-    @Autowired
-    private UserBackupNotifyRepository backupRepository;
-    @Autowired
-    private ContextUtil contextUtil;
-    @PostMapping("/save")
-    public Object save(UserBackupNotify backupNotify) {
-        APIResponse res = new APIResponse();
-        backupRepository.save(backupNotify);
-        res.setData(backupNotify);
-        res.setMessage("save success...");
-        res.setStatus(1);
-        return res;
-    }
-
-    @GetMapping("/list")
-    public Object list() {
-        APIResponse res = new APIResponse();
-        List<UserBackupNotify> getList = backupRepository.findAll();
-        res.setData(getList);
-        res.setMessage("List All...");
-        return res;
-    }
-
-    @PostMapping("/list/user")
-    public Object listUser(int user) {
-        APIResponse res = new APIResponse();
-        //Optional<UserEntity> dataUser = contextUtil.getUserDataFromContext();
-        List<UserBackupNotify> getList = backupRepository.findByUserId(user);
-        res.setStatus(1);
-        res.setData(getList);
-        res.setMessage("List User...");
-        return res;
-    }
-
-}
+//package com.powergroup.controller;
+//
+//import com.powergroup.model.bean.APIResponse;
+//import com.powergroup.model.service.UserBackupNotifyRepository;
+//import com.powergroup.model.table.UserBackupNotify;
+//import com.powergroup.util.ContextUtil;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping("/UserBackup")
+//public class UserBackupNotifyController {
+//    @Autowired
+//    private UserBackupNotifyRepository backupRepository;
+//    @Autowired
+//    private ContextUtil contextUtil;
+//    @PostMapping("/save")
+//    public Object save(UserBackupNotify backupNotify) {
+//        APIResponse res = new APIResponse();
+//        backupRepository.save(backupNotify);
+//        res.setData(backupNotify);
+//        res.setMessage("save success...");
+//        res.setStatus(1);
+//        return res;
+//    }
+//
+//    @GetMapping("/list")
+//    public Object list() {
+//        APIResponse res = new APIResponse();
+//        List<UserBackupNotify> getList = backupRepository.findAll();
+//        res.setData(getList);
+//        res.setMessage("List All...");
+//        return res;
+//    }
+//
+//    @PostMapping("/list/user")
+//    public Object listUser(int user) {
+//        APIResponse res = new APIResponse();
+//        //Optional<UserEntity> dataUser = contextUtil.getUserDataFromContext();
+//        List<UserBackupNotify> getList = backupRepository.findByUserId(user);
+//        res.setStatus(1);
+//        res.setData(getList);
+//        res.setMessage("List User...");
+//        return res;
+//    }
+//
+//}
