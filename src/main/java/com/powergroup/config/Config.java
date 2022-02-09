816d44a38627a60bec8b6e810aaf77c0dcf04905
sarayuth2019 <75245926+sarayuth2019@users.noreply.github.com>
2022-02-09 20:28:10 +0700

    เปิด path "/BankMarket/save"

--- src/main/java/com/powergroup/config/Config.java
+++ src/main/java/com/powergroup/config/Config.java
@@ -2,13 +2,14 @@ package com.powergroup.config;
 
 public class Config {
 
-  public static final String[] ALLOW_API_PATH = new String[]{
-      "/home/index",//อย่าลบ เอาไว้ตอน deploy
-      "/token_check",
-      "/authorizeUser",
-      "/authorizeMarket",
-      "/Register/user",
-      "/Register/market"
-  };
+    public static final String[] ALLOW_API_PATH = new String[]{
+            "/home/index",//อย่าลบ เอาไว้ตอน deploy
+            "/token_check",
+            "/authorizeUser",
+            "/authorizeMarket",
+            "/Register/user",
+            "/Register/market",
+            "/BankMarket/save"
+    };
 
 }
